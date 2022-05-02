3370d98d40b41ad26b42816047c52813c0e04000
ShubhamProjects <skpunk3695@gmail.com>
2022-05-03 00:12:38 +0530

    Modal styling

--- components/Modal.js
+++ components/Modal.js
@@ -7,19 +7,44 @@ const Modal = () => {
 	return (
 		<div
 			onClick={modalState}
-			className='bg-slate-100 fixed flex z-10 top-0 bottom-0 left-0 right-0
-		justify-center items-center bg-opacity-50'
+			className='bg-slate-300 fixed flex z-10 top-0 bottom-0 left-0 right-0
+		justify-center items-center bg-opacity-70'
 		>
 			<div
 				onClick={(e) => {
 					e.stopPropagation();
 				}}
-				className=' fixed top-[40%] left-[45%] flex-col p-14
-			border-2 border-red-500 -z-[10] flex drop-shadow-lg bg-white
-			rounded-md justify-center items-center'
+				className='fixed h-72 w-96
+			border-2 border-slate-300 -z-[10] drop-shadow-xl bg-white
+			rounded-lg'
 			>
-				<h1>I am modal</h1>
-				<button>Click me</button>
+				<img
+					src='https://links.papareact.com/ocw'
+					layout='fill'
+					objectFit='contain'
+					alt=''
+					className='border-b-2 h-44'
+				/>
+
+				<input
+					className='ml-4 mt-2 w-[91%]'
+					placeholder='Enter your caption...'
+				/>
+
+				<div className='flex justify-around mt-4'>
+					<button
+						className='rounded-lg hover:bg-blue-400 p-2 
+					border-2 w-40 bg-blue-100 font-semibold '
+					>
+						Select Photo
+					</button>
+					<button
+						className='rounded-lg hover:bg-blue-400 p-2 
+					border-2 w-40 bg-blue-100 font-semibold'
+					>
+						Post
+					</button>
+				</div>
 			</div>
 		</div>
 	);
